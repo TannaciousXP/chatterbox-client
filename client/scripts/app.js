59f045f1d85197af195dfb0a467e7fbf8d579e3e
Hack Reactor Student <student@hackreactor.com>
2017-05-26 20:50:08 -0700

    Added XSS filters

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -28,17 +28,6 @@ var app = {};
 app.server = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages';
 app.chat = $('#chats');
 
-app.checkRoom = function(obj) {
-  //grab the name of the room from obj.roomname
-   //check if the room exist, else
-  var room = $(`#roomSelect option[value = ${obj.roomname}]`);
-  console.log(room);
-  if (room[0]) {
-    return true;
-  }
-  
-  return false;
-};
 
 app.init = function() {
   // create an array to store all the fetch messages  
@@ -54,6 +43,13 @@ app.init = function() {
   //***set a see more chats***
   // var messages = app.fetch();
   // console.log(messages);
+  // var fetchNext = function() {
+  //   app.clearMessages();
+  //   app.fetch();
+  //   setTimeout(fetchNext, 3000);  
+  // };
+  // fetchNext();
+  app.fetch();
 };
 
 app.send = function(message) {
@@ -74,20 +70,37 @@ app.send = function(message) {
 };
 
 app.fetch = function() {
+
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'GET',
+    data: 'order=-createdAt',
     contentType: 'application/json',
     success: function (data) {
       console.log('fetch: received message,' + JSON.stringify(data.results, null, 2));
-      
+      for (let i = 0; i < 10; i++) {
+        // let name = data.results[i].username;
+        // let room = data.results[i].roomname;
+        // let text = data.results[i].text;
+        // if (name.includes('<script>') || text.includes('<script>') || room.includes('<script>')) {
+        //   messsage.username = 'Attempted Hack';
+        //   message.roomname = 'Attempted Hack';
+        //   message.text = 'Attempted Hack';
+        // }
+        
+        if (!app.hasRoom(data.results[i].roomname)) {
+          app.renderRoom(data.results[i].roomname);
+        }
+        
+        app.renderMessage(data.results[i]);
+      }
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/  console.error  
       console.error('fetch: Failed to receive message', data);
     }
-  });    
+  });
 };
 
 app.clearMessages = function() {
@@ -95,12 +108,14 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  var name = message.username;
-  var room = message.roomname;
-  var text = message.text;
-  var div = document.createElement('div');
-  div.innerText = name + ': ' + text;
-  $('#chats').append(div);
+  // if (message.username.includes('<script>') || message.text.includes('<script>') || message.roomname.includes('<script>')) {
+  //   messsage.username = 'Attempted Hack';
+  //   message.roomname = 'Attempted Hack';
+  //   message.text = 'Attempted Hack';
+  // }
+  // let userMessage = JSON.stringify(message.text);
+  // let userName = JSON.stringify(message.username);
+  $('#chats').append(`<div><a href="#" class="addUser">${xssFilters.inHTMLData(message.username)}</a>:<br> ${xssFilters.inHTMLData(message.text)}</div>`);
   // go to #roomSelect
     // check if roomname exist
     // if exist post in that roomchat
@@ -112,14 +127,36 @@ app.renderRoom = function(roomName) {
   opt.innerText = roomName;
   opt.value = roomName;
   $('#roomSelect').append(opt);
+
 };
 
+app.hasRoom = function(obj) {
+  // if (obj.roomname.length === 0 || obj.roomname === undefined) {
+  //   obj.roomname = 'main';
+  // }
+  var room = $(`#roomSelect option[value = ${obj.roomname}]`);
+  if (room[0]) {
+    return true;
+  }
+  return false;
+};
+
+// app.sanitizeInput = function(string) {
+//   //<script>
+//   //return 
+// };
 
 
 
 
+// check roomname exist
+  // if true 
+    // append message to roomname
+  // else 
+    // create room and append message to that room 
 
 
 
+// install npm install xss-filters
 
 
